a39797dcd945ad86eb9223a614691277aa08c85b
bpowarrior <alex.m@bpotel.com>
2017-10-24 16:03:43 +0300

    add test app

---
+++ app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+const todos = [
+	{
+		title: 'learn JS',
+		completed: true
+	},
+	{
+		title: 'learn Angular2',
+		completed: false
+	},
+	{
+		title: 'write app',
+		completed: false
+	}
+];
+
+@Component({
+	moduleId: module.id,
+	selector: 'app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css',]
+
+
+})
+export class AppComponent{
+	title = 'Angular 2Do'
+	todos = todos;
+
+	toggle(todo: any){
+		todo.completed = !todo.completed;
+	}
+
+	delete(todo: any){
+		let index = this.todos.indexOf(todo);
+		if(index>-1){
+			this.todos.splice(index, 1)
+		}
+	}
+}
