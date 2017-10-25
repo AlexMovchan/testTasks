ca891b52a83c2fe59ef88fa8356b9d9bfbdd4336
bpowarrior <alex.m@bpotel.com>
2017-10-25 11:00:33 +0300

    split app by components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,5 @@
 import { Component } from '@angular/core';
 
-class Todo { 
-	constructor(public title: string, 
-							public completed: boolean = false){}
-}
-
-const todos: ITodo[] = [
-	{
-		title: 'learn JS',
-		completed: true
-	},
-	{
-		title: 'learn Angular2',
-		completed: false
-	},
-	{
-		title: 'write app',
-		completed: false
-	}
-];
-
 @Component({
 	moduleId: module.id,
 	selector: 'app',
@@ -30,25 +10,4 @@ const todos: ITodo[] = [
 })
 export class AppComponent{
 	title: string = 'Angular 2Do'
-	todos: Todo [] = todos;
-	newTodoTitle: string = '';
-
-	create(event: event, title: string){
-		let todo: Todo = new Todo(this.newTodoTitle);
-		this.todos.push(todo);
-		this.newTodoTitle = '';
-	}
-
-	toggle(todo: Todo){		
-		todo.completed = !todo.completed;
-	}
-
-	delete(todo: Todo){
-		let index = this.todos.indexOf(todo);
-
-		if(index>-1){
-			this.todos.splice(index, 1)
-		}
-	}
-
 }
---
+++ app/shared/data.ts
@@ -0,0 +1,14 @@
+export const todos = [
+	{
+		title: 'learn JS',
+		completed: true
+	},
+	{
+		title: 'learn Angular2',
+		completed: false
+	},
+	{
+		title: 'write app',
+		completed: false
+	}
+];
